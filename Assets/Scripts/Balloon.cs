a1355d49359ecdbab3dcff2a4eaee05d8926b088
Matt <mm7f5@umsystem.edu>
2024-12-07 20:31:16 -0600

    worked on some balloon stuff

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -24,6 +24,8 @@ public class Balloon : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        throw new NotImplementedException();
+        Debug.Log(other.gameObject.name);
+        BeeBalloon.AddScore();
+        Destroy(this.gameObject);
     }
 }
