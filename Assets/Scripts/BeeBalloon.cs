a4d37bbd2a48a68f710e78617170fc6970c1ddee
Matt <mm7f5@umsystem.edu>
2024-12-08 23:39:44 -0600

    added logic to switch scenes. currently lives and scores are stored to player prefs (bad)

--- Assets/Scripts/BeeBalloon.cs
+++ Assets/Scripts/BeeBalloon.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BeeBalloon : MonoBehaviour
 {
@@ -14,22 +15,37 @@ public class BeeBalloon : MonoBehaviour
     private static int lives = 0;
     
     private static bool _isPaused = false;
-
+    private static List<String> _scenes = new List<String>{"Level1", "Level2" };
+    private static int currentScene = 0;
     public static int Score
     {
         get => score;
-        set => score = value;
+        set
+        {
+            score = value;
+            PlayerPrefs.SetInt("Score", score); // Save score to PlayerPrefs
+        }
     }
 
     public static int Lives
     {
         get => lives;
-        set => lives = value;
+        set
+        {
+            lives = value;
+            PlayerPrefs.SetInt("Lives", lives); // Save lives to PlayerPrefs
+        } 
     }
 
     private void Awake()
     {
         lives = defaultLives;
+        
+        score = PlayerPrefs.GetInt("Score", 0);
+        lives = PlayerPrefs.GetInt("Lives", 3);
+        
+        Debug.Log($"Score: {score}, Lives: {lives}");
+        
     }
 
     private void Update()
@@ -46,7 +62,9 @@ public class BeeBalloon : MonoBehaviour
         var allBalloons = FindObjectsOfType<Balloon>();
         if (allBalloons.Length <= 1)
         {
+            currentScene++;
             Debug.Log("All balloons popped");
+            SceneManager.LoadScene(_scenes[currentScene]);
         }
         
     }
